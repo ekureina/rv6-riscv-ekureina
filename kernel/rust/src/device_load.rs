246e2eba7179675ac3fd48dbfe8b3326669b3509
Claire Moore <kurennon@gmail.com>
2023-12-01 20:58:09 -0800

    Calculate the allowable amount of memory used
    Reserve enough space for the trampoline and Kenel stacks to have a
    double map in the PTE translation, but allow any value of memory up to
    this. This is 256 GiB, which means this only matters for massive
    computers or cloud servers (at least in 2023).

--- kernel/rust/src/device_load.rs
+++ kernel/rust/src/device_load.rs
@@ -11,10 +11,23 @@ pub static mut PHYSICAL_ADDRESS_STOP: c_bindings::uint64 = 0;
 #[no_mangle]
 pub unsafe extern "C" fn load_fdt(fdt_address: c_bindings::uint64) {
     let fdt = fdt::Fdt::from_ptr(fdt_address as *const u8).unwrap();
-    PHYSICAL_ADDRESS_STOP = fdt
+    // The true size of physical memory, calculated from the FDT
+    let true_physical_stop = fdt
         .memory()
         .regions()
         .fold(c_bindings::KERNBASE as usize, |mem_size, region| {
             mem_size + region.size.unwrap()
         }) as c_bindings::uint64;
+    // Get the CPU Count from the FDT. The max for this value for qemu's `virt` architecture is 8, but we allow for more memory to
+    // be used if less CPUs are allocated.
+    let cpu_count = fdt.cpus().count();
+    // Reserved pages for the Trampoline and Kernel stacks (2 for trampoline, and 2 per CPU (stack + guard page))
+    let reserved_pages = c_bindings::PGSIZE as usize * (2 * (cpu_count + 1));
+    // Set the `PHYSICAL_ADDRESS_STOP` to the minimum of the true amount of system RAM, and the maxiumum amount of
+    // physical RAM before Xv6 breaks this is about 256GiB, so this is probably unecessary, but just covering all
+    // the bases here
+    PHYSICAL_ADDRESS_STOP = core::cmp::min(
+        true_physical_stop,
+        c_bindings::MAXVA - reserved_pages as u64,
+    );
 }
