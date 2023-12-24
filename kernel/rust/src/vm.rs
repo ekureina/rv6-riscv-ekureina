2267ba7e8feb84be55f455eb62a37dd4e73877b2
Claire Moore <kurennon@gmail.com>
2023-12-23 20:23:35 -0800

    Phase one of COW Fork
    New Enum, RSW, setting the values of the RSW data. COWPage is the
    variant if a writable page was made readable.
    
    Can be used to fork the page if needed.

--- kernel/rust/src/vm.rs
+++ kernel/rust/src/vm.rs
@@ -1,12 +1,12 @@
-use bitfield::{bitfield, BitMut};
-use core::alloc::Layout;
+use bitfield::{bitfield, BitMut, BitRange};
+use num_enum::{FromPrimitive, IntoPrimitive};
 
 use crate::c_bindings;
 use crate::printf::{panic, printf};
 
 bitfield! {
     /// A wrapper around a Sv39 Page Table Entry
-    #[derive(PartialEq, Eq)]
+    #[derive(PartialEq, Eq, Copy, Clone)]
     #[repr(transparent)]
     pub struct PageTableEntry(u64);
     impl Debug;
@@ -27,7 +27,7 @@ bitfield! {
     /// Must be cleared by [`clear_dirty`]
     pub dirty, _: 7;
     /// The RSW field, used by rv6 to track COWs
-    pub u8, rsw, set_rsw: 9, 8;
+    pub u8, from into RSW, rsw, set_rsw: 9, 8;
     /// Physical Page Numbers to map to
     pub u8, into usize, ppn, set_ppn: 18, 10, 3;
 }
@@ -44,6 +44,16 @@ impl PageTableEntry {
     pub fn clear_dirty(&mut self) {
         self.0.set_bit(7, false);
     }
+
+    /// Map this PTE to a physical address
+    pub fn map_pa(&self) -> u64 {
+        (self.0 >> 10) << 12
+    }
+
+    /// Get the flag bits in this PTE
+    pub fn get_flags(&self) -> u64 {
+        self.bit_range(7, 0)
+    }
 }
 
 impl From<PageTableEntry> for u64 {
@@ -56,6 +66,17 @@ impl From<u64> for PageTableEntry {
     fn from(value: u64) -> Self {
         PageTableEntry(value)
     }
+}
+
+/// Values set in the RSW field of the [`PageTableEntry`]
+#[repr(u8)]
+#[derive(Debug, PartialEq, Eq, Default, Copy, Clone, IntoPrimitive, FromPrimitive)]
+pub enum RSW {
+    #[default]
+    /// Default value of the RSW
+    Default,
+    /// Set if the page in question is a COWable page (Writeable, but COW'd)
+    COWPage,
 }
 
 /// Prints out the mapped pages of the page table
@@ -100,56 +121,56 @@ pub unsafe extern "C" fn uvmcopy(
     size: u64,
 ) -> core::ffi::c_int {
     for va in (0..size).step_by(c_bindings::PGSIZE as usize) {
-        let old_pte = c_bindings::walk(old_pagetable, va, 0);
-        if old_pte.is_null() {
-            panic!("uvmcopy: pte should exist");
-        }
+        match unsafe {
+            c_bindings::walk(old_pagetable, va, 0)
+                .cast::<PageTableEntry>()
+                .as_mut()
+        } {
+            None => {
+                panic!("uvmcopy: pte should exist\0");
+            }
+            Some(old_pte) => {
+                if !old_pte.valid() {
+                    panic!("uvmcopy: page not present");
+                }
+                let writeable = old_pte.writeable();
+                if writeable {
+                    old_pte.set_rsw(RSW::COWPage);
+                    old_pte.set_writeable(false);
+                }
+                let pa = old_pte.map_pa();
+                let flags = old_pte.get_flags();
+                if unsafe {
+                    c_bindings::mappages(
+                        new_pagetable,
+                        va,
+                        u64::from(c_bindings::PGSIZE),
+                        pa,
+                        flags as i32,
+                    )
+                } != 0
+                {
+                    if writeable {
+                        old_pte.set_writeable(true);
+                        old_pte.set_rsw(RSW::COWPage);
+                    }
+                    return -1;
+                }
 
-        if (*old_pte & u64::from(c_bindings::PTE_V)) == 0 {
-            panic!("uvmcopy: page not present");
-        }
-
-        let pa = PTE2PA!(*old_pte);
-        let flags = PTE_FLAGS!(*old_pte);
-
-        let layout = Layout::from_size_align_unchecked(
-            c_bindings::PGSIZE as usize,
-            c_bindings::PGSIZE as usize,
-        );
-        let mem = alloc::alloc::alloc(layout);
-        if mem.is_null() {
-            c_bindings::uvmunmap(new_pagetable, 0, va / u64::from(c_bindings::PGSIZE), 1);
-            return -1;
-        }
-
-        core::ptr::copy_nonoverlapping(pa as *const u8, mem, c_bindings::PGSIZE as usize);
-        if c_bindings::mappages(
-            new_pagetable,
-            va,
-            u64::from(c_bindings::PGSIZE),
-            mem as u64,
-            flags as i32,
-        ) != 0
-        {
-            alloc::alloc::dealloc(mem, layout);
-            c_bindings::uvmunmap(new_pagetable, 0, va / u64::from(c_bindings::PGSIZE), 1);
-            return -1;
+                if writeable {
+                    match unsafe {
+                        c_bindings::walk(new_pagetable, va, 0)
+                            .cast::<PageTableEntry>()
+                            .as_mut()
+                    } {
+                        None => return -1,
+                        Some(new_pte) => {
+                            new_pte.set_rsw(RSW::COWPage);
+                        }
+                    }
+                }
+            }
         }
     }
     0
 }
-
-macro_rules! PTE2PA {
-    ($pte:expr) => {
-        (($pte) >> 10) << 12
-    };
-}
-
-macro_rules! PTE_FLAGS {
-    ($pte:expr) => {
-        $pte & 0x3FF
-    };
-}
-
-pub(crate) use PTE2PA;
-pub(crate) use PTE_FLAGS;
